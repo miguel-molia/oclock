064c798a08f000beb650e567377475a37cd56f74
miguel-molia <miguel.molia@laplateforme.io>
2023-04-28 13:29:34 +0200

    git all ok

--- chronometre.php
+++ chronometre.php
@@ -11,6 +11,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     
 </head>
 
@@ -60,9 +61,9 @@
 
 
     </footer>
-
+    
+<script src="chronometre.js"></script>
 </body>
 
 </html>
 
-<script src="chronometre.js"></script>
--- horloge.php
+++ horloge.php
@@ -15,6 +15,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&display=swap" rel="stylesheet">
 <link href="horloge.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -55,11 +56,11 @@
 
 
     </footer>
-
+<script src="horloge.js"></script>
 </body>
 
 </html>
 
 
-<script src="horloge.js"></script>
+
 
